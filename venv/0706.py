1b97492d78e55bbca2c890e385aacd4544f172c8
parkjunghwan91 <qkr911125@naver.com>
2021-07-09 10:15:23 +0900

    no message

--- venv/0706.py
+++ venv/0706.py
@@ -157,32 +157,101 @@ print(b)
 #     if coffee==0:
 #         print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
 #         break
+#
+# marks=[90,25,67,45,80]
+#
+# number=0
+# for mark in marks:
+#     number=number+1
+#     if mark >= 60:
+#         print("%d번 학생은 합격입니다."%number)
+#     else:
+#         print("%d번 학생은 불합격입니다"%number)
+#
+#
+# add=0
+# for i in range(1,11):
+#     add=add+i
+#
+# print(add)
+#
+# marks = [90,25,68,54,80]
+# for number in range(len(marks)):
+#     if marks[number] < 60:
+#         continue
+#     print("%d번 학생 축하합니다 합격입니다~"% (number+1))
+#
+#
+# for i in range(2,10):
+#     for j in range(1,10):
+#         print(i*j,end=" ")
+#     print('')
+#
+# for i in range(2,10):
+#     for j in range(1,10):
+#         print("%d*%d=%2d" %(i,j,i*j),end=" ")
+#     print('')
+#
+# a=[1,2,3,4]
+# print([num*2 for num in a])
 
-marks=[90,25,67,45,80]
+# result=[x*y for x in range(2,10)
+#         for y in range(1,10) ]
+# print(result)
+#
+# def add_mul(choice, *args):
+#     if choice=="add":
+#         result=0
+#         for i in args:
+#             result=result+i
+#     elif choice=="mul":
+#         result=1
+#         for i in args:
+#             result=result+i
+#     return  result
 
-number=0
-for mark in marks:
-    number=number+1
-    if mark >= 60:
-        print("%d번 학생은 합격입니다."%number)
-    else:
-        print("%d번 학생은 불합격입니다"%number)
+class Calculator:
+    def __init__(self):
+        self.result=0
+
+    def add(self,num):
+        self.result += num
+        return  self.result
+cal1=Calculator()
+cal2=Calculator()
+
+print(cal1.add(3))
+print(cal1.add(4))
+print(cal2.add(3))
+print(cal2.add(7))
 
 
-add=0
-for i in range(1,11):
-    add=add+i
+class FourCal:
+    def setdata(self,first,second):
+        self.first=first
+        self.second=second
+    def add(self):
+        result=self.first + self.second
+        return  result
+    def mul(self):
+        result=self.first * self.second
+        return result
+    def sub(self):
+        result=self.first - self.second
+        return result
+    def div(self):
+        result=self.first / self.second
+        return result
 
-print(add)
-
-marks = [90,25,68,54,80]
-for number in range(len(marks)):
-    if marks[number] < 60:
-        continue
-    print("%d번 학생 축하합니다 합격입니다~"% (number+1))
-
-
-for i in range(2,10):
-    for j in range(1,10):
-        print(i*j,end=" ")
-    print('')
+a=FourCal()
+b=FourCal()
+a.setdata(4,2)
+b.setdata(3.8)
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
+print(b.add())
+print(b.mul())
+print(b.sub())
+print(b.div())
